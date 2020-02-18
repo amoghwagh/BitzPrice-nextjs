ba0dd35eef4eeca640b118c61e185968c3b3d875
Amogh Wagh <amoghwagh@gmail.com>
2020-02-18 14:26:11 +0530

    Added a layout

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,12 @@
-import Navbar from "../components/Navbar";
+import Layout from "../components/Layout";
 
 const Index = () => {
   return (
-    <div>
-      <Navbar />
-      <h1>Welcome to BitzPrice</h1>
-    </div>
+    <Layout>
+      <div>
+        <h1>Welcome to BitzPrice</h1>
+      </div>
+    </Layout>
   );
 };
 
