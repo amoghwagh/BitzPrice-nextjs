0ed2899665deee92519853d71ee6ac2a914c5df8
Amogh Wagh <amoghwagh@gmail.com>
2020-02-18 14:42:18 +0530

    Added async loading of coin desk api

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,23 @@
+import fetch from "isomorphic-unfetch";
 import Layout from "../components/Layout";
 
-const Index = () => {
+const Index = props => {
   return (
     <Layout>
       <div>
         <h1>Welcome to BitzPrice</h1>
+        {props.bpi.time.updated}
       </div>
     </Layout>
   );
 };
 
+Index.getInitialProps = async () => {
+  const res = await fetch("https://api.coindesk.com/v1/bpi/currentprice.json");
+  const data = await res.json();
+
+  return {
+    bpi: data 
+  };
+};
 export default Index;
